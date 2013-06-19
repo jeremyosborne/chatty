2e2af24312e5676476b6e052241b1f1e76c0966a
neo.orz.hk <neo@orz.hk>
2013-06-19 18:43:52 +0800

    Update server.js
    use OPENSHIFT_NODEJS_IP and OPENSHIFT_NODEJS_PORT if OPENSHIFT_INTERNAL_PORT or OPENSHIFT_INTERNAL_IP is unavailable.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var PORT = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
-var IPADDRESS = process.env.OPENSHIFT_INTERNAL_IP || '127.0.0.1';
+var PORT = process.env.OPENSHIFT_INTERNAL_PORT || process.env.OPENSHIFT_NODEJS_PORT  || 8080;
+var IPADDRESS = process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 var express = require('express');
 var server;
